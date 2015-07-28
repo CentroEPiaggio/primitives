1f9b7243786fbbecc95c700066864c62576c4e3f
Danilo Caporale <danilo.caporale@gmail.com>
2015-07-28 22:57:54 +0200

    Search tree grows in 2 dimensions. Slight fix in init.m file (still awful though).

--- src/init.m
+++ src/init.m
@@ -2,4 +2,7 @@ disp('Cerca la cartella tbxmanager in cui sono installati i files della libreria
 keyboard
 cd ../../tbxmanager
 startup
-cd ../primitives/srcbx
+cd ../primitives/src
+
+addpath(pwd) % add current dir to path
+addpath('cprnd/');
