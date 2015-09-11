53b7f03552bd7ee64843e43dbbc8ccd5a5073ea1
Danilo Caporale <danilo.caporale@gmail.com>
2015-09-11 16:30:21 +0200

    Fixed ZMP calculation.

--- example/parinit.m
+++ example/parinit.m
@@ -10,10 +10,11 @@ q0 = rand(nq,1)
 % q0(3)=1;
 % qp0 = rand(nq,1)
 q0 = [0,deg2rad(90),1];
-q0 = [0,deg2rad(10),1];
+q0 = [0,deg2rad(10),2];
 qref0=q0;
 
 len_ic = 1;
+prim_filepath='prim/';
 
 % parameters
 m1 = 100;
@@ -73,7 +74,7 @@ primitive_muovi_params = struct('name','muovi',    ...
     'xf_vec_len',xf_vec_len, ...
     'vx0_vec_len',vx0_vec_len,  ...
     'vxf_vec_len',vxf_vec_len, ...
-    'filepath','prim/' ...
+    'filepath',prim_filepath ...
     )
 primitive_abbassa_params = struct('name','abbassa',    ...
     'yi',yi,            ...
@@ -82,22 +83,22 @@ primitive_abbassa_params = struct('name','abbassa',    ...
     'Tend',Tend,        ...
     'Ts',Ts,            ...
     'yf_vec_len',yf_vec_len,    ...
-    'filepath','prim/' ...
+    'filepath',prim_filepath ...
     )
 make_primitives_muovi(primitive_muovi_params)
 make_primitives_abbassa(primitive_abbassa_params)
-return
+
 %% Run the RSim Compiled Simulation Using New Signal Data
 close all
 disp('Starting batch simulations.')
 
 % trick for speeding up the parfor loops: define variables as local, hence
 % not using a shared structure.
-y_len=primitive_muovi_params.yf_vec_len;
+y_len=primitive_abbassa_params.yf_vec_len;
 xf_len=primitive_muovi_params.xf_vec_len;
 vx0_len=primitive_muovi_params.vx0_vec_len;
 vxf_len=primitive_muovi_params.vxf_vec_len;
-filepath=primitive_muovi_params.filepath;
+
 
 % main for loop
 tic
@@ -107,9 +108,23 @@ for i =1:len_ic
             for vi = 1:vx0_len
                 for vf = 1:vxf_len
                     % Bang out and run the next set of data with RSim
-                    test_codenum = [num2str(x),'_',num2str(y),'_',num2str(vi),'_',num2str(vf)];
-                    primitiva_muovi_loadstr = [filepath, 'primitiva_muovi_',test_codenum,'.mat'];
-                    runstr = ['.', filesep, 'modello -f rsim_tfdata.mat=',primitiva_muovi_loadstr,' -p params',num2str(i),'.mat -o run_muovi_',test_codenum,'.mat -v -tf ',num2str(Tend)];
+%                     the next 2 lines if in a single file there are both
+%                     primitives stored together. After those two lines we
+%                     build a new file with the primitives taken separately
+%                     and merged together
+%                     test_codenum = [num2str(x),'_',num2str(y),'_',num2str(vi),'_',num2str(vf)];
+%                     primitiva_muovi_loadstr = [prim_filepath, 'primitiva_muovi_',test_codenum,'.mat'];
+test_codenum_muovi = [num2str(x),'_',num2str(vi),'_',num2str(vf)];
+test_codenum_abbassa = [num2str(y)];
+primitiva_muovi_loadstr = [prim_filepath, 'primitiva_muovi_',test_codenum_muovi,'.mat'];
+primitiva_abbassa_loadstr = [prim_filepath, 'primitiva_abbassa_',test_codenum_abbassa,'.mat'];
+muovi=load(primitiva_muovi_loadstr);
+abbassa=load(primitiva_abbassa_loadstr);
+q_reference = [muovi.q_reference(1,:);
+    muovi.q_reference(2,:);
+    abbassa.q_reference(2,:)];
+save('runna.mat','q_reference');
+                    runstr = ['.', filesep, 'modello -f rsim_tfdata.mat=runna.mat -p params',num2str(i),'.mat -o run_muovi_',test_codenum_muovi,'_',test_codenum_abbassa,'.mat -v -tf ',num2str(Tend)];
 % runstr = ['.', filesep, 'modello -f rsim_tfdata.mat=',primitiva_muovi_loadstr,' -p params',num2str(i),'.mat -o run_muovi_',test_codenum,'.mat -v -tf 0.4'];
                     [status, result] = system(runstr);
                     if status ~= 0, error(result); end
