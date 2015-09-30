e0fb1c982246c9fa5f237c899dc771e7292dee8f
Corrado Pavan <corradopav@gmail.com>
2015-09-30 10:00:34 +0200

    fixed reconnect (still buggy visualization)

--- src/ReConnect.m
+++ src/ReConnect.m
@@ -4,13 +4,19 @@ function [T,G,E] = ReConnect(idx_new,idx_near,T,G,E,prim,q,cost)
 z_near = T.get(idx_near);
 z_near = fix_nans(z_near,prim.dimensions);
 % re-insert the z_near node attaching it to the z_new node
-%% TODO redesign this part:
 % instead of InsertNode try to play with the parents of the node
 % assigning a new parent to an existing node
-[T,G,E] = InsertNode(idx_new, z_near, T, G, E, prim, q,cost);
-% remove node
-T = T.removenode(idx_near);
+idx_old = T.getparent(idx_near);
+T.Parent(idx_near) = idx_new;
+% remove old edge
+E{idx_old,idx_near} = [];
+G(idx_old,idx_near) = 0;
 
-% update G and E
-%% TODO
+G(idx_new,idx_near) = cost;
+actions = struct('source_node', idx_new,...
+        'dest_node', idx_near,...
+        'primitive',prim.name,...
+        'primitive_q',q);
+    E{idx_new,idx_near} = actions;
+
 
