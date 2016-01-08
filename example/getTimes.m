95e2f1e775daf8d3dded76075df716a68bc8bbd7
Danilo Caporale <danilo.caporale@gmail.com>
2016-01-08 10:11:28 +0100

    Function to extract time intervals for a time-optimal jerk-limited profile.

---
+++ example/getTimes.m
@@ -0,0 +1,14 @@
+function Times = getTimes(A,D,J,x,xhat,xbar)
+keyboard
+%% definition of times
+T1 = A/J;
+T2 = x-2*T1;
+T3 = T1;
+T4 = xhat;
+T5 = D/J;
+T6 = xbar-2*T5;
+T7 = T5;
+Times = [T1 T2 T3 T4 T5 T6 T7];
+fprintf('%6s %6s %6s %6s %6s %6s %6s \n','T1','T2','T3','T4','T5','T6','T7');
+fprintf('%6.2f %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f \n',T1,T2,T3,T4,T5,T6,T7);
+end
