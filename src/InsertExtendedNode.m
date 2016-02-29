0bcbfbf5f8a7dfac419220d56d8753185368aa0e
Danilo Caporale <danilo.caporale@gmail.com>
2016-02-29 01:43:19 +0100

    wip

---
+++ src/InsertExtendedNode.m
@@ -0,0 +1,6 @@
+function InsertExtendedNode
+[added_node,T,G,E,... % inputs for algorithm stuff
+    plot_nodes,plot_edges, ... % inputs for plotting visual stuff
+    idx_last_added] ... % return index of last added node
+    = InsertNode(idx_current, z_new,  T,     G,     E, prim, q,     cost, x, time, ... % inputs for algorithm stuff
+end
