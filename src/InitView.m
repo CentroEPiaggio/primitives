04cb66a99377b3d5208d4f116b91663da7c7b422
Danilo Caporale <danilo.caporale@gmail.com>
2016-02-29 15:36:30 +0100

    Fixed bug.

--- src/InitView.m
+++ src/InitView.m
@@ -12,10 +12,11 @@ if verbose
     plot(z_init(1),z_init(2),'go','linewidth',4) % plot initial point
     plot(z_goal(1),z_goal(2),'ko','linewidth',4) % plot initial point
     xlabel('x [m]'); ylabel('y [m]'); % TODO: parametrize inside the primitive
-    for i=1:numel(Obstacles.Node)
-        Obstacles.Node{i}.P.plot('color','black','alpha',0.5);
+    if obstacles_on
+        for i=1:numel(Obstacles.Node)
+            Obstacles.Node{i}.P.plot('color','black','alpha',0.5);
+        end
     end
-    
     figure(fig_xy)
     plot3(z_init(1),z_init(2),0,'go','linewidth',4) % plot initial point % HARDFIX 0 in z to represent tau
     hold on
@@ -24,11 +25,13 @@ if verbose
     xlabel('x axis [m]'); ylabel('y axis [m]'); zlabel('tau [1]'); % TODO: parametrize inside the primitive
     % plot obstacles
     figure(fig_chi0)
-%     obstacle_speed_limit.P.plot('color','black','alpha',1);
+    %     obstacle_speed_limit.P.plot('color','black','alpha',1);
     if multiple_primitives
         figure(fig_xy)
         Chi1.P.plot('color','lightblue','alpha',0.5); hold on;
-        obstacle_wall.P.plot('color','black','alpha',0.5);
+        if obstacles_on
+            obstacle_wall.P.plot('color','black','alpha',0.5);
+        end
         for ii=1:length(bias_points)
             plot3(bias_points{ii}(1),bias_points{ii}(2),bias_points{ii}(3),'*','color','yellow','linewidth',10)
         end
