498a6244aab1d8fe953b2c38096b10e23d3dab86
Alessandro <ale.settimi@gmail.com>
2016-03-04 13:09:37 +0100

    added script to visualize path and make a video of the planning phase

---
+++ src/plan/video_planning.m
@@ -0,0 +1,78 @@
+close all
+
+InitView
+
+close(fig_chi0)
+
+frames=1;
+formatOut = 'yyyy_mm_dd_hh_MM_SS';
+str_date=datestr(now,formatOut);
+vidObj=VideoWriter(['plan_' str_date '.avi']);
+vidObj.Quality = 100;
+vidObj.FrameRate = 2;
+open(vidObj);
+
+view([-50 30])
+hold on
+
+movie_anima(frames) = getframe(figure(fig_xy));
+frames = frames +1;
+
+for i=1:numel(T.Node)
+    node = T.Node{i};
+    if isnan(node(5))
+        node(5)=0;
+    end
+    plot3(node(1),node(2),node(5),'rx','linewidth',2);
+
+    parent_id = T.Parent(i);
+    if parent_id==0
+        continue
+    end
+    parent = T.Node{parent_id};
+    if isnan(parent(5))
+        parent(5)=0;
+    end
+    
+    plot3([node(1) parent(1)],[node(2) parent(2)],[node(5) parent(5)],'b','linewidth',2);
+
+    movie_anima(frames) = getframe(figure(fig_xy));
+    frames = frames +1;
+end
+
+opt_plan=extract_plan(T,E,opt_path);
+path_length = opt_plan.nnodes;
+
+t = [];
+x = [];
+time_offset = 0;
+
+for ii=2:path_length
+    t_ii = time_offset + opt_plan.Node{ii}.time;
+    x_ii = opt_plan.Node{ii}.x;
+    t = [t, t_ii];
+    time_offset = t(end);
+    x = [x x_ii];
+end
+
+x(5,isnan(x(5,:))>0)=0;
+plot3(x(1,:),x(2,:),x(5,:),'k-d','linewidth',4);
+movie_anima(frames) = getframe(figure(fig_xy));
+frames = frames +1;
+movie_anima(frames) = getframe(figure(fig_xy));
+frames = frames +1;
+movie_anima(frames) = getframe(figure(fig_xy));
+frames = frames +1;
+
+disp('saving video...');
+
+for iter=1:frames-1
+       vidObj.writeVideo(movie_anima(iter));
+end
+vidObj.writeVideo(movie_anima(iter));
+   
+close(vidObj);
+   
+disp('...done!');
+
+close(fig_xy)
