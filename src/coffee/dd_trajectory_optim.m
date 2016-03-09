93b7660fef121457f06f8044de15de2d00f79709
Danilo Caporale <danilo.caporale@gmail.com>
2016-03-09 15:53:19 +0100

    Fixed bug.

--- src/coffee/dd_trajectory_optim.m
+++ src/coffee/dd_trajectory_optim.m
@@ -12,27 +12,35 @@ time = [];
 
 % Tf = 20;
 N = 20;
-u_1 = sdpvar(1,N);
-u_2 = sdpvar(1,N);
+% N=21;
+u_1 = sdpvar(1,N+1);
+u_2 = sdpvar(1,N+1);
 x_0 = [x0(:); 0];
 x_f = [xf(:); 0];
-x_var = sdpvar(5,N);
+x_var = sdpvar(5,N+1);
 Tf = sdpvar(1,1);
 objective = 0;
 
 cost=Inf;
 retval = 1;
 
-Constraints_control = [u_1<=100, u_1>=-100, u_2<=100, u_2>=-100];
-Constraints_state = [x_var(:,1) == x_0.*[1;1;1;Tf;Tf], x_var(:,end) == x_f.*[1;1;1;Tf;Tf]];
 v_max = 0.1;
 w_max = 1;
-% Constraints_state = [Constraints_state, -pi <= x_var(3,:) <= pi, -v_max*0 <= x_var(4,:) <= v_max, -w_max <= x_var(5,:) <= w_max];
-Constraints_state = [Constraints_state, -pi <= x_var(3,:) <= pi, -v_max*0*Tf <= x_var(4,:) <= v_max*Tf, -Tf*w_max <= x_var(5,:) <= w_max*Tf, 0.1<=Tf<=30]
+
+Constraints_control = [u_1<=100, u_1>=-100, u_2<=100, u_2>=-100];
+% Constraints_state = [x_var(:,1) == x_0.*[1;1;1;Tf;Tf], x_var(:,end) == x_f.*[1;1;1;Tf;Tf]];
+Constraints_state = [x_var(:,1) == x_0.*[1;1;1;1;1], x_var(:,end) == x_f.*[1;1;1;1;1]];
+% Constraints_state = [x_var(:,1) == x_0.*[1;1;1;1;1], x_var(:,end) == x_f.*[1/(Tf*v_max);1/(Tf*v_max);1/(Tf*v_max);Tf;Tf]];
+% Constraints_state = [x_var(:,1) == x_0.*[1;1;1;Tf;Tf], x_var(:,end) == x_f.*[1/Tf/v_max;Tf;Tf;Tf;Tf]];
+% Constraints_state = [x_var(:,1) == x_0.*Tf, x_var(:,end) == x_f.*Tf]; % 
+
+Constraints_state = [Constraints_state, -pi <= x_var(3,:) <= pi, -v_max*0 <= x_var(4,:) <= v_max, -w_max <= x_var(5,:) <= w_max, 0.1<=Tf<=30];
+% Constraints_state = [Constraints_state, -pi <= x_var(3,:) <= pi, -v_max*0*Tf <= x_var(4,:) <= v_max*Tf, -Tf*w_max <= x_var(5,:) <= w_max*Tf, 0.1<=Tf<=30]
+
 
 
 h = 1/N; % passo di integrazione
-for k = 1:N-1
+for k = 1:N
     funz = [x_var(4,k)*cos(x_var(3,k)); % x dot
         x_var(4,k)*sin(x_var(3,k)); % y dot
         x_var(5,k); % theta dot
@@ -44,7 +52,8 @@ for k = 1:N-1
 end
 objective = Tf;
 Global_constraints = [Constraints_control, Constraints_state];
-diagnostics = optimize(Global_constraints,objective,sdpsettings('solver','fmincon','fmincon.MaxIter',200,'fmincon.TolFun',0.1));
+diagnostics = optimize(Global_constraints,objective,sdpsettings('solver','fmincon','fmincon.MaxIter',200,'fmincon.TolFun',0.00001));
+
 
 if diagnostics.problem ~= 0 % FAIL STATE
     retval = 0;
@@ -57,10 +66,31 @@ Tfin = double(Tf);
 u(1,:) = u_1;
 u(2,:) = double(x_var(5,:));
 x = double(x_var(1:4,:));
-x(4,:) = x(4,:)/Tfin;
+% x = x/Tfin;
+% x(4,:) = x(4,:)/Tfin;
+% x(1,:) = x(1,:)*Tfin*v_max;
+% x(2,:) = x(2,:)*Tfin*v_max;
+% x(3,:) = x(3,:)*Tfin*w_max;
+% keyboard
+%%
+% figure
+% plot(0:1/N:1,double(x_var(1,:)))
+% grid on
+% hold on
+% plot(0:1/N:1,double(x_var(4,:)))
 
-time = 0:h*Tfin:h*(N-1)*Tfin;
+% figure
+% plot(time,double(x_var(1,:)))
+% grid on
+% hold on
+% plot(time,double(x_var(4,:))/Tfin)
+% plot(time,cumtrapz(time,double(x_var(4,:)/Tfin)))
+%%
+% time = 0:h*Tfin:h*(N-1)*Tfin;
+
 % time = 0:h:h*(N-1);
+
+time = 0:h*Tfin:Tfin;
 
 if debug
     figure(16)
