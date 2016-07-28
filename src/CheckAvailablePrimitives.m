19cde8e15ab8e537c8d1f36a88ba562455962ba1
Danilo Caporale <danilo.caporale@gmail.com>
2016-07-28 15:45:35 +0200

    Fixed bug.

--- src/CheckAvailablePrimitives.m
+++ src/CheckAvailablePrimitives.m
@@ -1,6 +1,7 @@
-function idx_P_avail = CheckAvailablePrimitives(z_new,Ptree)
+function idx_P_avail = CheckAvailablePrimitives(z_new,Ptree,idx_z_new_sampling_space)
+% keyboard
 idx_P_avail = zeros(Ptree.nnodes,1);
-z_check = fix_nans(z_new,Ptree.Node{1}.dimensions);  % conventionally Chi0 is the image space of the FIRST element of the primitive tree
+z_check = fix_nans(z_new,Ptree.Node{idx_z_new_sampling_space}.dimensions_imagespace);  % conventionally Chi0 is the image space of the FIRST element of the primitive tree
 % for ii=2:Ptree.nnodes % conventionally Chi0 is the image space of the FIRST element of the primitive tree. Hence no need to check the first element
 for ii=1:Ptree.nnodes % check among all available primitives
     prim = Ptree.Node{ii};
